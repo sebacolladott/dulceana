36eadbce6cd2e80149f0b42dea7ba4f9eaf2a5e4
Sebastian Collado <sebastiancolladott@gmail.com>
2024-06-23 15:31:10 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import { ErrorPage } from "./error-page";
+import { Root } from "./routes/root";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "", element: <></> }],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
 // Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})
+window.ipcRenderer.on("main-process-message", (_event, message) => {
+  console.log("message", message);
+});
